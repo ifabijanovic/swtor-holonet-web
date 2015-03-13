403b6c76e80afd8b68245c404cffc3a4d11229b7
Ivan Fabijanovic <ivan-fabijanovic@live.com>
2015-03-13 20:32:13 +0100

    Implemented simple page for sending push notifications.

--- cloud/app.js
+++ cloud/app.js
@@ -4,12 +4,18 @@ var app = express();
 
 // Modules
 var authentication = require('cloud/authentication/module.js');
+var push = require('cloud/push/module.js');
+
+var modules = [authentication, push];
 
 // Configuration and module initialisation
 app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
 app.use(express.bodyParser());    // Middleware for reading request body
-authentication.init(app, express);
+
+for (var i = 0; i < modules.length; i++) {
+	modules[i].init(app, express);
+};
 
 // Routes
 app.get('/', function(req, res) {
@@ -23,7 +29,9 @@ app.get('/', function(req, res) {
     }
 });
 
-authentication.registerRoutes(app);
+for (var i = 0; i < modules.length; i++) {
+	modules[i].registerRoutes(app);
+};
 
 app.use(function(req, res) {
     res.render('404');
